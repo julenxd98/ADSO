9a460713d96c5cfde492c47d1a4ee3630fcb49e4
julenfernandez <julenfernandez123@gmail.com>
2019-11-21 16:55:07 +0100

    Casi lo tenemos

--- scriptbueno.sh
+++ scriptbueno.sh
@@ -1,44 +1,84 @@
 #!/bin/bash
-let uid
-let ou
-let dc
-let extension
-let csv_path
-OUTPUT="/tmp/csv.tmp"
-
-dibujarMenu() {
+#Declaramos las variables globales
+$uid
+$ou
+$dc
+$extension
+$csv_path
+$todo 
+echo "uid="$uid",ou="$ou",dc="$dc",dc="$extension",csv"$csv_path=$todo
+#Creamos el menu
+function dibujarMenu(){
+	OUTPUT="/tmp/csv.tmp"
         dialog  --title "Parseador CSV a LDIF" \
                 --ok-label "Aceptar" \
                 --cancel-label "Cancelar" \
                 --menu "$MSGBOX" 20 0 20 \
-               Nombre  "Indica el nombre del admin OpenLDAP" \
-               OU  "Indica la unidad organizativa" \
-               Servidor "Indica el nombre del servidor" \
-               Extension  "Indica la extensión del servidor" \
-               CSV  "Indica el nombre del fichero CSV" \
-               Salir "Salir del script" \
-		2> /tmp/csv.$$		
+               1  "Indica el nombre del admin OpenLDAP" \
+               2  "Indica la unidad organizativa" \
+               3 "Indica el nombre del servidor"  \
+               4  "Indica la extensión del servidor" \
+               5  "Indica el nombre del fichero CSV" \
+               6 "Guardar salida"  \
+	       0 "Salir" 2>/tmp/salida.tmp
+		a=$(cat /tmp/salida.tmp)
+		respose=$?
+		rm /tmp/csv.tmp
 }
-
-crearuid() {
-	dialog	--title "Nombre del administrador" \
+function pedirAdmin(){
+		 OUTPUT="/tmp/input.txt"	
+		 dialog	--title "Nombre del administrador" \
 		--ok-label "Aceptar" \
 		--cancel-label "Cancelar" \
-		--inputbox "$2" 8 60 "$uid" \
-		2> /tmp/csv.tmp.$$
-             uid=$(cat /tmp/csv.tmp.$$)
+		--inputbox "Indica el nombre del administrador"8 60 2>$OUTPUT
+		echo "por ahora llevas" $todo 
+		uid=$(<$OUTPUT)
+		rm /tmp/input.txt
 }
-crearou() {
-                        ou=$(cat /tmp/csv.tmp.$$)
-}
-creardc() {
-                     dc=$(cat /tmp/csv.tmp.$$)
-}
-crearext() {
-		extension=$(cat /tmp/csv.tmp.$$)
-}
-creartodo() {
-	todo="uid="$uid",ou="$ou",dc="$dc",dc="$extension
+function pedirOu(){ 
+		 OUTPUT="/tmp/input.txt"
+		dialog       --title "Nombre de la unidad organizativa" \
+                --ok-label "Aceptar" \
+                --cancel-label "Cancelar" \
+                --inputbox "Indica el nombre de la unidad organizativa"8 60 2>$OUTPUT
+		echo "por ahora llevas" $todo 
+		ou=$(<$OUTPUT)
+		rm /tmp/input.txt
+		}
+
+function pedirDomain(){ 
+		OUTPUT="/tmp/input.txt"
+		dialog       --title "Nombre del dominio" \
+                --ok-label "Aceptar" \
+                --cancel-label "Cancelar" \
+                --inputbox "Indica el nombre del dominio"8 60 2>$OUTPUT
+		echo "por ahora llevas" $todo 
+		dc=$(<$OUTPUT)
+		rm /tmp/input.txt
 }
 
-dibujarMenu
+function pedirExt(){ 
+		OUTPUT="/tmp/input.txt"
+		dialog       --title "Nombre de la extension" \
+                --ok-label "Aceptar" \
+                --cancel-label "Cancelar" \
+                --inputbox "Indica la extension del dominio"8 60 2>$OUTPUT
+		echo "por ahora llevas" $todo 
+		extension=$(<$OUTPUT)
+		rm /tmp/input.txt
+function pedirCsv(){
+    	OUTPUT="/tmp/input.txt"
+    	dialog --title "El nombre del archivo CSV a parsear" \
+    	--backtitle "Parseador de CSV a LDIP" \
+    	--inputbox "Indica el nombre del archivo CSV que está en esta carpeta para parsear" 8 60 2>$OUTPUT
+    	echo "por ahora llevas" $todo 
+    	csv_path=$(<$OUTPUT)
+    	rm /tmp/input.txt
+}
+function salida(){
+	$todo >> salida.ldif
+}
+	while true
+	do
+		dibujarMenu
+	done
